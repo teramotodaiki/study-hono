75937c72a094fd7f4793f2ae82163759ebad745d
teramotodaiki <i101323@gmail.com>
2023-06-03 16:49:41 +0900

    add debug route

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => c.text("Hello Hono!"));
+app.all("*", (c) => c.text(`You've reached ${c.req.url}`));
 
 export default app;
